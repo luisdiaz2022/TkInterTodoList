69e22dabb76ed80c271c455ca220b8d7c2dba913
Luis <luisedgucv@gmail.com>
2023-10-29 19:48:57 -0500

    Added interface and a function to add Todo

--- db.py
+++ db.py
@@ -11,7 +11,7 @@ cursor = conn.cursor()
 
 cursor.execute("""
     CREATE TABLE if not exists todo(
-               id INTERGET PRIMARY GEY AUTOINCREMENT,
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
                created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                description TEXT NOT NULL,
                completed BOOLEAN NOT NULL
@@ -19,3 +19,28 @@ cursor.execute("""
 """)
 
 conn.commit()
+
+def addTodo():
+    todo = entry.get()
+    cursor.execute("""
+                    INSERT INTO todo (description, completed) VALUES (?, ?)
+                   """, (todo, False))
+    conn.commit()
+    entry.delete(0, END)
+
+label = Label(root, text='Tarea')
+label.grid(row=0, column=0)
+
+entry = Entry(root, width=40)
+entry.grid(row=0, column=1)
+
+btn = Button(root, text='Agregar', command=addTodo)
+btn.grid(row=0, column=0)
+
+frame = LabelFrame(root, text='Mis Tareas', padx=5, pady=5)
+frame.grid(row=1, column=0, columnspan=3, sticky='nswe', padx=5)
+
+entry.focus()
+
+root.bind('<Return>', lambda x:addTodo())
+root.mainloop()
