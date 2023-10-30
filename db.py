d62f4ced9a5c807cbd087c2eb49057e218723845
Luis <luisedgucv@gmail.com>
2023-10-29 20:32:57 -0500

    rendering the elements on the app

--- db.py
+++ db.py
@@ -20,13 +20,29 @@ cursor.execute("""
 
 conn.commit()
 
+def render_todos():
+    rows = cursor.execute("SELECT * FROM todo").fetchall()
+    print(rows)
+
+    for i in range(0, len(rows)):
+        completed = rows[i][3]
+        description = rows[i][2]
+        checkbtn = Checkbutton(frame, text=description, width=42, anchor='w')
+        checkbtn.grid(row=i, column=0, sticky='w')
+
+
 def addTodo():
     todo = entry.get()
-    cursor.execute("""
+    if todo:
+        cursor.execute("""
                     INSERT INTO todo (description, completed) VALUES (?, ?)
                    """, (todo, False))
-    conn.commit()
-    entry.delete(0, END)
+        conn.commit()
+        entry.delete(0, END)
+        render_todos()
+    else:
+        pass
+    
 
 label = Label(root, text='Tarea')
 label.grid(row=0, column=0)
@@ -35,12 +51,14 @@ entry = Entry(root, width=40)
 entry.grid(row=0, column=1)
 
 btn = Button(root, text='Agregar', command=addTodo)
-btn.grid(row=0, column=0)
+btn.grid(row=0, column=2)
 
 frame = LabelFrame(root, text='Mis Tareas', padx=5, pady=5)
 frame.grid(row=1, column=0, columnspan=3, sticky='nswe', padx=5)
 
 entry.focus()
+
+render_todos()
 
 root.bind('<Return>', lambda x:addTodo())
 root.mainloop()
